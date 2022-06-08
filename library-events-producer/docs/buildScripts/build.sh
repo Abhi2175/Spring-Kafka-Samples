6d1e16edcf5c4d9d8e9e075febc75e035bc5547f
Abhishek Kumar <abhi.kumar2175@gmail.com>
2022-06-08 14:31:50 +0530

    Initial commit

---
+++ library-events-producer/docs/buildScripts/build.sh
@@ -0,0 +1,6 @@
+#docker-compose -f docker-compose-dependencies.yaml down
+#docker-compose -f docker-compose-dependencies.yaml up -d
+
+
+docker-compose -f Multiple-kafka-docker-compose.yaml down
+docker-compose -f Multiple-kafka-docker-compose.yaml up -d
