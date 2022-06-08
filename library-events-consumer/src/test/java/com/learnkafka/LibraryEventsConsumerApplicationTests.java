802bb32f97b759077c31a7706dad35086a835d7a
Abhishek Kumar <abhi.kumar2175@gmail.com>
2022-06-08 18:19:57 +0530

    Library-event-consumer base project added

---
+++ library-events-consumer/src/test/java/com/learnkafka/LibraryEventsConsumerApplicationTests.java
@@ -0,0 +1,11 @@
+package com.learnkafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryEventsConsumerApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
